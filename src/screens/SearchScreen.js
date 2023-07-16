97e9b2dda1340c9677be3377a284149ec6480d25
Lesroy Wedderburn <wlesroy98@outlook.com>
2023-07-16 10:17:19 +0100

    Completion of API addition

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../Components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessgae, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'London'
+                }
+        });
+        setResults(response.data.businesses);
+    } catch (err) {
+        setErrorMessage('Something went wrong')
+        }
+    };
 
     return (
         <View>
-            <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitted')}
+            <SearchBar 
+            term={term} 
+            onTermChange={setTerm}
+            onTermSubmit={() => searchApi(term)}
             />
-            <Text>Search Screen</Text>
+           {errorMessgae ? <Text>{errorMessgae}</Text> : null}
+            <Text>We have found {results.length}</Text>
         </View>
-    );
+    )
 };
 
 const styles = StyleSheet.create({});
